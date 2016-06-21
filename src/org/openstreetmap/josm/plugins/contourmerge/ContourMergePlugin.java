4af647608eb1431ea85efa89ddcf6fcf729ed371
Gubaer <karl.guggisberg@guggis.ch>
2016-06-21 18:16:53 +0200

    reformat

--- src/org/openstreetmap/josm/plugins/contourmerge/ContourMergePlugin.java
+++ src/org/openstreetmap/josm/plugins/contourmerge/ContourMergePlugin.java
@@ -3,35 +3,33 @@ package org.openstreetmap.josm.plugins.contourmerge;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 
-
-
 /**
  * <strong>ContourMergePlugin</strong> is the main class for the
  * <tt>contourmerge</tt> plugin.
  */
-public class ContourMergePlugin extends Plugin{
+public class ContourMergePlugin extends Plugin {
 
     public ContourMergePlugin(PluginInformation info) {
-		super(info);
+        super(info);
         ContourMergeModelManager.getInstance().wireToJOSM();
         ContourMergeView.getInstance().wireToJOSM();
-	}
+    }
 
-	static private boolean modeEnabled;
+    static private boolean modeEnabled;
 
-	static public ContourMergeModelManager getModelManager() {
-		return ContourMergeModelManager.getInstance();
-	}
+    static public ContourMergeModelManager getModelManager() {
+        return ContourMergeModelManager.getInstance();
+    }
 
-	static public ContourMergeView getView() {
-		return ContourMergeView.getInstance();
-	}
+    static public ContourMergeView getView() {
+        return ContourMergeView.getInstance();
+    }
 
-	static public boolean isEnabled(){
-		return modeEnabled;
-	}
+    static public boolean isEnabled() {
+        return modeEnabled;
+    }
 
-	static public void setEnabled(boolean enabled){
-		modeEnabled = enabled;
-	}
+    static public void setEnabled(boolean enabled) {
+        modeEnabled = enabled;
+    }
 }
